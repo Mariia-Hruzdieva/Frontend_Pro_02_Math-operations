e0494438aeb90c7a79660775fa00de1b258f7401
Mariia Hruzdieva <mhruzdieva@gmail.com>
2023-10-02 18:45:00 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-function myFunction() {
-    let a = +prompt("Enter first number:", 2);
-    let b = +prompt("Enter second number:", 2);
+let a;
+let b;
+let c;
 
-    alert(a + '+' + b + '=' + (a + b)
-        + '\n' +
-        a + '-' + b + '=' + (a - b)
-        + '\n' +
-        a + '*' + b + '=' + (a * b)
-        + '\n' +
-        a + '/' + b + '=' + (a / b)
-    );
+a = +prompt('Введіть перше число');
+if (isNaN(a)) {
+    alert('Значення не є числом');
+} else {
+    b = +prompt('Введіть друге число');
+    if (isNaN(b)) {
+        alert('Значення не є числом');
+    } else {
+        c = +prompt('Введіть третє число');
+        if (isNaN(c)) {
+            alert('Значення не є числом');
+        } else {
+            const res = (a + b + c) / 3;
+            alert('Середнє арифметичне = ' + res);
+        }
+    }
 }
+
+
+
+
+
+
